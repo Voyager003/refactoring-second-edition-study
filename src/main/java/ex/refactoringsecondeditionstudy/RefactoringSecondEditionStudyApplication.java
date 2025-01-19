d3b727f2773d5b0cc12dec6a3a3b3f7fdb2a0f06
voyager003 <miraclex000@gmail.com>
2025-01-19 15:05:16 +0900

    Initial commit

---
+++ src/main/java/ex/refactoringsecondeditionstudy/RefactoringSecondEditionStudyApplication.java
@@ -0,0 +1,13 @@
+package ex.refactoringsecondeditionstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringSecondEditionStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringSecondEditionStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ex/refactoringsecondeditionstudy/RefactoringSecondEditionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.refactoringsecondeditionstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringSecondEditionStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
