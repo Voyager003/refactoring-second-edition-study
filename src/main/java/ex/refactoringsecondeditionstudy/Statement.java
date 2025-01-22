0c24d3885d7d1907bd2da9f4e075d16c324cee74
voyager003 <miraclex000@gmail.com>
2025-01-22 16:15:50 +0900

    refactor: amountFor() 메서드가 중간 데이터(StatementData)를 이용하도록 변경

--- src/main/java/ex/refactoringsecondeditionstudy/Statement.java
+++ src/main/java/ex/refactoringsecondeditionstudy/Statement.java
@@ -4,17 +4,17 @@ package ex.refactoringsecondeditionstudy;
 public class Statement {
     public String statement(Invoice invoice, Plays plays) throws Exception {
         StatementData statementData = new StatementData(invoice, plays);
-        return renderPlainText(statementData, plays);
+        return renderPlainText(statementData);
     }
 
-    private String renderPlainText(StatementData statementData, Plays plays) throws Exception {
+    private String renderPlainText(StatementData statementData) throws Exception {
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", statementData.getCustomer()));
         for (Performance performance : statementData.getPerformances()) {
-            result.append(String.format("%s: $%d %d석\n", statementData.playFor(performance).getName(), amountFor(performance, plays) / 100, performance.getAudience()));
+            result.append(String.format("%s: $%d %d석\n", statementData.playFor(performance).getName(), statementData.amountFor(performance) / 100, performance.getAudience()));
         }
 
-        result.append(String.format("총액: $%d\n", totalAmount(statementData.getInvoice(), plays)));
-        result.append(String.format("적립 포인트: %d점", totalVolumeCredits(statementData.getInvoice(), plays)));
+        result.append(String.format("총액: $%d\n", statementData.totalAmount()));
+        result.append(String.format("적립 포인트: %d점", statementData.totalVolumeCredits()));
         return result.toString();
     }
 
--- src/main/java/ex/refactoringsecondeditionstudy/StatementData.java
+++ src/main/java/ex/refactoringsecondeditionstudy/StatementData.java
@@ -22,4 +22,54 @@ public class StatementData {
     public Play playFor(Performance performance) {
         return plays.getPlayById(performance.getPlayId());
     }
+
+    public int amountFor(Performance performance) throws Exception {
+        int result;
+        switch (playFor(performance).getType()) {
+            case TRAGEDY:
+                result = 40000;
+                if (performance.getAudience() > 30) {
+                    result += 1000 * (performance.getAudience() - 30);
+                }
+                break;
+            case COMEDY:
+                result = 30000;
+                if (performance.getAudience() > 20) {
+                    result += 10000 + 500 * (performance.getAudience() - 20);
+                }
+                result += 300 * performance.getAudience();
+                break;
+            default:
+                throw new Exception("알 수 없는 장르");
+        }
+        return result;
+    }
+
+    public int totalAmount() throws Exception {
+        int totalAmount = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            totalAmount += amountFor(performance);
+        }
+        return totalAmount / 100;
+    }
+
+    public int totalVolumeCredits() {
+        int volumeCredit = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            volumeCredit += volumeCreditFor(performance);
+        }
+        return volumeCredit;
+    }
+
+    private int volumeCreditFor(Performance performance) {
+        int result = 0;
+
+        result += Math.max(performance.getAudience() - 30, 0);
+
+        if (playFor(performance).getType().equals(PlayType.COMEDY)) {
+            result += Math.floor(performance.getAudience() / 5);
+        }
+
+        return result;
+    }
 }
