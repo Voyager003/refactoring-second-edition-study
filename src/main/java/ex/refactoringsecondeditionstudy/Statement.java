f7c5a20648b0f48395b02b8ad273bd8b6d4f0fc6
voyager003 <miraclex000@gmail.com>
2025-01-22 16:18:37 +0900

    feat: renderHtml 버전 추가

--- src/main/java/ex/refactoringsecondeditionstudy/Statement.java
+++ src/main/java/ex/refactoringsecondeditionstudy/Statement.java
@@ -18,6 +18,20 @@ public class Statement {
         return result.toString();
     }
 
+    private String renderHtml(StatementData statementData) throws Exception {
+        StringBuilder result = new StringBuilder(String.format("<h1> 청구내역 (고객명: %s)\n </h1>", statementData.getCustomer()));
+        result.append("<table> \n");
+        result.append("<tr><th> 연극 </th> <th>좌석 수</th> <th>금액</th>");
+        for (Performance performance : statementData.getPerformances()) {
+            result.append(String.format("<tr><td> %s: </td> <td> $%d </td> <td> %d석 </td></tr>\n", statementData.playFor(performance).getName(), statementData.amountFor(performance) / 100, performance.getAudience()));
+        }
+        result.append("</table>\n");
+
+        result.append(String.format("총액: $%d\n", statementData.totalAmount()));
+        result.append(String.format("적립 포인트: %d점", statementData.totalVolumeCredits()));
+        return result.toString();
+    }
+
     private int totalAmount(Invoice invoice, Plays plays) throws Exception {
         int totalAmount = 0;
         for (Performance performance : invoice.getPerformances()) {
