d89f24a60281ab7b4ed9934b0567a984ae586531
voyager003 <miraclex000@gmail.com>
2025-01-22 14:32:23 +0900

    chore: starter-json 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation ("org.projectlombok:lombok")
+    implementation ("org.springframework.boot:spring-boot-starter-json")
 }
 
 tasks.withType<Test> {
