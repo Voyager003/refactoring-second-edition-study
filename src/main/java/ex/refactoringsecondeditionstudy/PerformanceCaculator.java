04934c039e8bd752386b627fdc46578a8cb71dfe
voyager003 <miraclex000@gmail.com>
2025-01-22 21:18:41 +0900

    refactor: amountFor() 메서드 옮기기
    함수를 클래스로 옮기는 함수 옮기기 기법(move function)

---
+++ src/main/java/ex/refactoringsecondeditionstudy/PerformanceCaculator.java
@@ -0,0 +1,32 @@
+package ex.refactoringsecondeditionstudy;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PerformanceCaculator {
+    private Performance performance;
+    private Play play;
+
+    public int amountFor() throws Exception {
+        int result;
+        switch (play.getType()) {
+            case TRAGEDY:
+                result = 40000;
+                if (performance.getAudience() > 30) {
+                    result += 1000 * (performance.getAudience() - 30);
+                }
+                break;
+            case COMEDY:
+                result = 30000;
+                if (performance.getAudience() > 20) {
+                    result += 10000 + 500 * (performance.getAudience() - 20);
+                }
+                result += 300 * performance.getAudience();
+                break;
+            default:
+                throw new Exception("알 수 없는 장르");
+        }
+        return result;
+    }
+
+}
