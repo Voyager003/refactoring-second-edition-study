92eabfd536ee4b94e26b7e65dcd53e22feba8f39
voyager003 <miraclex000@gmail.com>
2025-02-13 16:59:11 +0900

    feat: 3.2 중복코드 리팩터링 후
    함수 추출

--- src/main/java/ex/refactoringsecondeditionstudy/ch03/Order.java
+++ src/main/java/ex/refactoringsecondeditionstudy/ch03/Order.java
@@ -8,16 +8,16 @@ public class Order {
     private double itemPrice;
 
     public double calculateTotalPrice() {
-        double basePrice = quantity * itemPrice;
-        double discount = 0;
-        if (quantity > 100) {
-            discount = basePrice * 0.1;
-        }
+        double basePrice = getBasePrice();
+        double discount = (quantity > 100) ? basePrice * 0.1 : 0;
         return basePrice - discount;
     }
 
     public double calculateTax() {
-        double basePrice = quantity * itemPrice;
-        return basePrice * 0.1;
+        return getBasePrice() * 0.1;
+    }
+
+    private double getBasePrice() {
+        return quantity * itemPrice;
     }
 }
