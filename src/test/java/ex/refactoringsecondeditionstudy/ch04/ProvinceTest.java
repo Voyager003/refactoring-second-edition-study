7bb8f6ec3bd2984c821ed12f50c4f505a67e1908
voyager003 <miraclex000@gmail.com>
2025-02-20 20:32:27 +0900

    test: 첫 번째 테스트 케이스

--- src/test/java/ex/refactoringsecondeditionstudy/ch04/ProvinceTest.java
+++ src/test/java/ex/refactoringsecondeditionstudy/ch04/ProvinceTest.java
@@ -2,10 +2,22 @@ package ex.refactoringsecondeditionstudy.ch04;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ProvinceTest {
     private static final DataLoader DATA_LOADER = new DataLoader(
             ProvinceTest.class.getClassLoader(),
             new ObjectMapper()
     );
+
+    @Test
+    void shortfall() {
+        final Province asia = new Province(DATA_LOADER.sampleProvinceData());
+
+        final int shortfall = asia.getShortfall();
+
+        assertThat(shortfall).isEqualTo(5);
+    }
 }
